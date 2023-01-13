d3bbbe79de79a3ed9a3d3e82784a816f067624c6
Can Guven <cguven@wpi.edu>
2023-01-13 14:38:42 -0500

    added name for Can and Tim

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -1,7 +1,8 @@
 package edu.wpi.teamname;
 
 public class Main {
-
+System.out.println("Can Guven")
+System.out.println("Tim Stecko")
   public static void main(String[] args) {
     App.launch(App.class, args);
   }
