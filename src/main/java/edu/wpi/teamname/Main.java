84c60b3ba1acfd0c58b53ed5605af6d106c9fbd9
Can Guven <cguven@wpi.edu>
2023-01-13 14:45:17 -0500

    added branch name

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -3,6 +3,8 @@ package edu.wpi.teamname;
 public class Main {
 System.out.println("Can Guven")
 System.out.println("Tim Stecko")
+
+System.println.out("Branch - CanTim ")
   public static void main(String[] args) {
     App.launch(App.class, args);
   }
