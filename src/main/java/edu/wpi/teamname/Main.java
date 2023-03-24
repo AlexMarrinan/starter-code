a78b70daf92e6bd958c43c9b218f57ab20ccf514
Valerie-Childers <vgchilders@WPI.edu>
2023-03-24 12:01:36 -0400

    ah

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -2,10 +2,8 @@ package edu.wpi.teamname;
 
 public class Main {
 
-
-  System.out.println("Nate Wood");
   public static void main(String[] args) {
     App.launch(App.class, args);
-    System.out.println("Thea");
+    System.out.println("Valerie");
   }
 }
