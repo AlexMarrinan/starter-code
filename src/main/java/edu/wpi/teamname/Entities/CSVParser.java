a46e1ae0c545b8554f288de97035f6a0168d1192
Valerie-Childers <vgchilders@WPI.edu>
2023-03-31 10:33:23 -0400

    Ian wake upppppppp

--- src/main/java/edu/wpi/teamname/Entities/CSVParser.java
+++ src/main/java/edu/wpi/teamname/Entities/CSVParser.java
@@ -1,3 +1,25 @@
 package edu.wpi.teamname.Entities;
 
-public class CSVParser {}
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class CSVParser {
+
+    public void parseFile(String filePath){
+        try {
+            getTemps(filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<String> getTemps(String filePath) throws IOException, SQLException{
+
+        return null;
+    }
+
+}
